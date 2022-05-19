814cbe622485687754211f43554087adb990f840
Rodrigo Rebelo <rrebelo553@gmail.com>
2022-05-19 15:42:31 -0300

    feat: header component added

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,14 @@
 import { AppProps } from 'next/app'
+import { Header } from '../components/Header'
 
 import '../styles/global.scss'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-      <Component {...pageProps} />
+      <>
+        <Header/>
+        <Component {...pageProps} />
+      </>
   )
 }
 
