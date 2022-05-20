11da2f4cd6fa0693ad9a4abb325e51cab38e4dbe
Rodrigo Rebelo <rrebelo553@gmail.com>
2022-05-20 12:05:17 -0300

    feat: login and auth system added

--- src/components/SignInButton/index.tsx
+++ src/components/SignInButton/index.tsx
@@ -1,18 +1,22 @@
 import { FaGithub } from 'react-icons/fa'
 import { FiX } from 'react-icons/fi'
+import { signIn, useSession, signOut } from 'next-auth/react'
+
 import styles from './styles.module.scss'
 
 export function SignInButton(){
-  const isUserLoggedIn = true;
+    const { data: session } = useSession()
 
-  return isUserLoggedIn ? (
-    <button className={styles.signInButton} type="button">
+    console.log(session)
+
+  return session ? (
+    <button className={styles.signInButton} type="button" onClick={() => signOut()}>
     <FaGithub color="#04D361" />
-    Rodrigo Rebelo
+    {session.user.name}
     <FiX color="#737380" className={styles.closeIcon} />
   </button>
   ) : (
-    <button className={styles.signInButton} type="button">
+    <button className={styles.signInButton} type="button" onClick={() => signIn('github')}>
     <FaGithub color="#EBA417" />
     Sing in With GitHub
   </button>
