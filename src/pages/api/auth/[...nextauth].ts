23e115dfe961f63b97f40f625ba1b83d6f198061
Rodrigo Rebelo <rrebelo553@gmail.com>
2022-05-20 16:09:15 -0300

    feat: integrated with faunaDB

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,8 +1,10 @@
 import NextAuth from "next-auth"
 import GithubProvider from "next-auth/providers/github"
 
+import { query as q } from "faunadb"
+import { fauna } from '../../../services/fauna'
+
 export default NextAuth({
-  // Configure one or more authentication providers
   providers: [
     GithubProvider({
       clientId: process.env.GIT_HUB_CLIENT_ID,
@@ -13,6 +15,41 @@ export default NextAuth({
         }
       }
     }),
-    // ...add more providers here
   ],
+    callbacks: {
+    async signIn({ user, account, profile}){
+      const { email } = user;
+
+      try {
+        await fauna.query(
+          q.If(
+            q.Not(
+              q.Exists(
+                q.Match(
+                  q.Index('user_by_email'),
+                  q.Casefold(user.email)
+                )
+              )
+            ), 
+            q.Create(
+              q.Collection('users'),
+              { data: { email }}
+            ),
+            q.Get(
+              q.Match(
+                q.Index('user_by_email'),
+                q.Casefold(user.email)
+              )
+            )
+          )
+        )
+
+        return true
+      } catch {
+        return false
+      }
+
+    },
+  }
+
 })
