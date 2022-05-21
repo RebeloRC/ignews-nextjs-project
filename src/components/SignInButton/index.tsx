66b05131f120a4929ee70b3b53703e3030cfe19e
Rodrigo Rebelo <rrebelo553@gmail.com>
2022-05-21 15:16:05 -0300

    fix: customer duplication fixed

--- src/components/SignInButton/index.tsx
+++ src/components/SignInButton/index.tsx
@@ -7,8 +7,6 @@ import styles from './styles.module.scss'
 export function SignInButton(){
     const { data: session } = useSession()
 
-    console.log(session)
-
   return session ? (
     <button className={styles.signInButton} type="button" onClick={() => signOut()}>
     <FaGithub color="#04D361" />
